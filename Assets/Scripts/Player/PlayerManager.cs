6cda7921d5b90baaf7adaf8842e7efc957c6f8d9
PCx200 <petko05raychinov@gmail.com>
2025-05-11 18:09:22 +0300

    Main camera attaches to the player after spawning

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -13,11 +13,22 @@ public class PlayerManager : MonoBehaviour
 
         PlayerController player = playerClone.GetComponent<PlayerController>();
         RegisterPlayer(player);
+
+        // After Spawning the player, the main camera is being put as a child of the player object
+        SetCamera(playerClone);
     }
 
     public void RegisterPlayer(PlayerController playerController)
     {
         mouseClickController.OnClick.RemoveAllListeners();
         mouseClickController.OnClick.AddListener(playerController.GoToDestination);
+    }
+
+    private void SetCamera(PlayerController player)
+    {
+        Camera mainCamera = Camera.main;
+        mainCamera.transform.SetParent(player.transform);
+        mainCamera.transform.localPosition = new Vector3(0, 9, -7);
+        mainCamera.transform.rotation = Quaternion.Euler(35, 0, 0);
     }
 }
